bb2018384f49d5502f5ff1ee4636befd6697e902
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-06-24 00:45:51 +0300

    updated test

--- redirect.php
+++ redirect.php
@@ -26,7 +26,7 @@ try {
             'accepturl' => getSelfUrl() . '/accept.php',
             'cancelurl' => getSelfUrl() . '/cancel.php',
             'callbackurl' => getSelfUrl() . '/callback.php',
-            'test' => 1,
+            'test' => 0,
         ]);
     } else {
         echo 'Invalid request method.';
