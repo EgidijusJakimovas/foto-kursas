a03677eb55b50544aee4834f739a0fd0e29999a0
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-06-24 00:35:49 +0300

    updated redirect

--- redirect.php
+++ redirect.php
@@ -1,25 +1,38 @@
 <?php
- 
-require_once('WebToPay.php');
- 
-try {
-    $self_url = 'https://foto-kursas-930ec9144443.herokuapp.com/';
 
-    $request = WebToPay::redirectToPayment([
-        'projectid' => 244531,
-        'sign_password' => 'Labaslabas123*',
-        'orderid' => 'max-max-'.rand(1000000, 9999999),
-        'p_email' => $_POST['elpastas'],
-		'p_firstname' => $_POST['vardas'],
-		'p_lastname' => $_POST['pavarde'],
-        'amount' => 489,
-        'currency' => 'EUR',
-        'country' => 'LT',
-        'accepturl' => self_url() . '/accept.php',
-        'cancelurl' => self_url() . '/cancel.php',
-        'callbackurl' => self_url() . '/callback.php',
-        'test' => 1,
-    ]);
+require_once('WebToPay.php');
+
+function getSelfUrl(): string
+{
+    return 'http://127.0.0.1:5500';
+}
+
+try {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $vardas = $_POST['vardas'] ?? '';
+        $pavarde = $_POST['pavarde'] ?? '';
+        $elpastas = $_POST['elpastas'] ?? '';
+
+        $self_url = 'https://foto-kursas-930ec9144443.herokuapp.com/';
+
+        WebToPay::redirectToPayment([
+            'projectid' => 244531,
+            'sign_password' => 'Labaslabas123*',
+            'orderid' => 'max-max-' . rand(1000000, 9999999),
+            'p_email' => $elpastas,
+            'p_firstname' => $vardas,
+            'p_lastname' => $pavarde,
+            'amount' => 489,
+            'currency' => 'EUR',
+            'country' => 'LT',
+            'accepturl' => getSelfUrl() . '/accept.php',
+            'cancelurl' => getSelfUrl() . '/cancel.php',
+            'callbackurl' => getSelfUrl() . '/callback.php',
+            'test' => 1,
+        ]);
+    } else {
+        echo 'Invalid request method.';
+    }
 } catch (Exception $exception) {
     echo get_class($exception) . ':' . $exception->getMessage();
 }
