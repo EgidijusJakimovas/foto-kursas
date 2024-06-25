5aee958c6d17815bc7665fc1d885a8f2934af441
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-06-25 18:01:42 +0300

    updated order id

--- redirect.php
+++ redirect.php
@@ -8,16 +8,26 @@ function getSelfUrl(): string
     return 'https://foto-kursas-930ec9144443.herokuapp.com';
 }
 
+// Generate a unique auto-incrementing order ID
+function generateOrderId(): string
+{
+    static $lastOrderId = 1000000; // Initial value for the order ID
+    $lastOrderId++;
+    return 'max-max-' . $lastOrderId;
+}
+
 try {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $vardas = $_POST['vardas'] ?? '';
         $pavarde = $_POST['pavarde'] ?? '';
         $elpastas = $_POST['elpastas'] ?? '';
 
+        $orderId = generateOrderId();
+
         WebToPay::redirectToPayment([
             'projectid' => 244570,
             'sign_password' => '7ada0f6b4ace81a594c33bc2545246f7',
-            'orderid' => 'max-max-' . rand(1000000, 9999999),
+            'orderid' => $orderId,
             'p_email' => $elpastas,
             'p_firstname' => $vardas,
             'p_lastname' => $pavarde,
@@ -33,3 +43,4 @@ try {
 } catch (Exception $exception) {
     echo get_class($exception) . ':' . $exception->getMessage();
 }
+?>
