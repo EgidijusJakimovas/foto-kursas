dd422f4daae9a3cbabd47973cf1b71667cf766fc
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-06-26 18:09:50 +0300

    added DB

--- const.php
+++ const.php
@@ -1,19 +1,18 @@
 <?php
-	// paysera connection information
-	define('PAYSERA_PROJECT_ID', 		);
-	define('PAYSERA_PASSWORD', 			'');
-	define('PAYSERA_CURRENCY', 			'EUR');
+    // paysera connection information
+    define('PAYSERA_PROJECT_ID', '244570');
+    define('PAYSERA_PASSWORD', '7ada0f6b4ace81a594c33bc2545246f7');
+    define('PAYSERA_CURRENCY', 'EUR');
 
-	// database connection information
-	define('DB_NAME', 		'');
-	define('DB_HOST', 		'localhost');
-	define('DB_USERNAME', 	'');
-	define('DB_PASSWORD', 	'');
+    // database connection information
+    define('DB_NAME', 'n9teib9it8m8u2z3');
+    define('DB_HOST', 'b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
+    define('DB_USERNAME', 'vo3l7cqkori4bdkn');
+    define('DB_PASSWORD', 'vciaqvbz60v0z7jy');
 
-	// database tables information
-	define('DB_TABLE_PAYMENTS', 					'orders');
-	define('DB_TABLE_PAYMENTS_COLUMN_UNIKALUS', 	'id');
-	define('DB_TABLE_PAYMENTS_COLUMN_LAIKAS', 		'data');
-	define('DB_TABLE_PAYMENTS_COLUMN_UZSAKYMAS', 	'order_id');
-
+    // database tables information
+    define('DB_TABLE_ORDERS', 'orders');
+    define('DB_TABLE_ORDERS_COLUMN_ID', 'id');
+    define('DB_TABLE_ORDERS_COLUMN_TIMESTAMP', 'data');
+    define('DB_TABLE_ORDERS_COLUMN_ORDER_ID', 'order_id');
 ?>
--- redirect.php
+++ redirect.php
@@ -1,44 +1,104 @@
 <?php
-
-require_once('WebToPay.php');
-
-function getSelfUrl(): string
-{
-    return 'https://foto-kursas-930ec9144443.herokuapp.com';
-}
-
-function generateOrderId(): string
-{
-    static $lastOrderId = 0;
-    $lastOrderId++;
-    return str_pad($lastOrderId, 6, '0', STR_PAD_LEFT);
-}
+require_once 'WebToPay.php';
+require_once 'const.php';
 
 try {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $vardas = $_POST['vardas'] ?? '';
-        $pavarde = $_POST['pavarde'] ?? '';
-        $elpastas = $_POST['elpastas'] ?? '';
+    // Database info
+    $host       = 'b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+    $user       = 'vo3l7cqkori4bdkn';
+    $pass       = 'vciaqvbz60v0z7jy';
+    $database   = 'n9teib9it8m8u2z3';
+    
+    // Database table info
+    $table          = 'orders';
+    $orderID        = 'id';
+    $name           = 'name';
+    $surname        = 'surname';
+    $email          = 'email';
+    $phone          = 'phone';
+    $paymentStatus  = 'payment_status';
+    $paidSum        = 'paid_sum';
+    $data           = 'data';
 
-        $orderId = generateOrderId();
-
-        WebToPay::redirectToPayment([
-            'projectid' => 244570,
-            'sign_password' => '7ada0f6b4ace81a594c33bc2545246f7',
-            'orderid' => $orderId,
-            'p_email' => $elpastas,
-            'p_firstname' => $vardas,
-            'p_lastname' => $pavarde,
-            'amount' => 100,
-            'currency' => 'EUR',
-            'country' => 'LT',
-            'accepturl' => getSelfUrl() . '/accept.php',
-            'cancelurl' => getSelfUrl() . '/cancel.php',
-            'callbackurl' => getSelfUrl() . '/callback.php',
-            'test' => 0,
-        ]);
+    // Custom PDO options.
+    $options = array(
+        PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES  => false
+    );
+    
+    // Connect to MySQL and instantiate our PDO object.
+    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $pass, $options);
+    
+    // Create our INSERT SQL query.
+    $sql = "INSERT INTO $table (`$orderID`, `$name`, `$surname`, `$email`, `$phone`, `$paymentStatus`, `$paidSum`, `$data`) VALUES (:id, :name, :surname, :email, :phone, :payment_status, :paid_sum, :data)";
+    
+    // Prepare our statement.
+    $statement = $pdo->prepare($sql);
+    
+    // GET MAX ID FOR MAKING ORDER ID
+    $pdo2 = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $pass, $options);
+    $data2 = $pdo2->prepare("SELECT MAX(id) as id FROM $table LIMIT 1;");
+    $data2->execute();
+    $row2 = $data2->fetch();
+    
+    // Make sure only 1 result is returned
+    if($data2->rowCount() == 1){
+        $id_from_db = $row2['id'] + 1; // because always returns id - 1
+        $order_id_from_db = '0000' . strval($id_from_db);
     }
+
+    // Bind user's entered values in form to our arguments
+    $orderID        = $order_id_from_db;
+    $name           = $_POST['name'];
+    $surname        = $_POST['surname'];
+    $email          = $_POST['email'];
+    $phone          = $_POST['phone'];
+    $paymentStatus  = 0; // because user has not paid yet, he will pay only on callback.php
+    
+    // Against SQL injections
+    $statement->bindValue(':id',            $orderID);
+    $statement->bindValue(':name',          $name);
+    $statement->bindValue(':surname',       $surname);
+    $statement->bindValue(':email',         $email);
+    $statement->bindValue(':phone',         $phone);
+    $statement->bindValue(':payment_status',$paymentStatus);
+    $statement->bindValue(':paid_sum',      0); // Initial value, since the payment is not done yet
+    $statement->bindValue(':data',          date("Y-m-d H:i:s")); // Assuming current timestamp
+
+    // Execute the statement and insert our values.
+    $inserted = $statement->execute();
+
+    // Because PDOStatement::execute returns a TRUE or FALSE value,
+    // we can easily check to see if our insert was successful.
+    // if($inserted){
+        // echo 'Row inserted!<br>';
+    // }
+
+    // PAYSERA PAYMENT
+    function getSelfUrl(): string {
+        return 'https://foto-kursas-930ec9144443.herokuapp.com';
+    }
+
+    WebToPay::redirectToPayment([
+        'projectid'     => 244570,
+        'sign_password' => '7ada0f6b4ace81a594c33bc2545246f7',
+        'orderid'       => $orderID,
+        // 'amount'       => $paidSum * 100, // returning cents (for paysera) from euros (from DB)
+        'amount'        => 100,
+        'currency'      => 'EUR',
+        'country'       => 'LT',
+        'p_firstname'   => $name,
+        'p_lastname'    => $surname,
+        'p_email'       => $email,
+        'p_phone'       => $phone,
+        'accepturl'     => getSelfUrl() . '/accept.php',
+        'cancelurl'     => getSelfUrl() . '/cancel.php',
+        'callbackurl'   => getSelfUrl() . '/callback.php',
+        'test'          => 0,
+    ]);
 } catch (Exception $exception) {
+    echo "SQL exception on 'go.php' file. Enable error-reporting for more info.";
+    //TODO: HIDE IT IN PRODUCTION
     echo get_class($exception) . ':' . $exception->getMessage();
 }
 ?>
