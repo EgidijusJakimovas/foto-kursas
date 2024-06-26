372b53c1bf58bff7b16c16d68a48396b49eee219
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-06-26 20:00:05 +0300

    updated accept

--- accept.php
+++ accept.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
     <title>Redirecting...</title>
     <script type="text/javascript">
         setTimeout(function() {
             window.location.href = "index.html";
-        }, 5000); // 5000 milliseconds = 5 seconds
+        }, 10000); // 10000 milliseconds = 10 seconds
     </script>
 </head>
 <body>
-    Thank you for buying. You will be redirected in 5 seconds.
+    <p>Thank you for your purchase! We have received your order and are processing it. Please check your email for payment confirmation and further instructions. You will be redirected to our homepage in 10 seconds.</p>
 </body>
 </html>
