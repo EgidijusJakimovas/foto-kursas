b43e4f0a72de2428c7dd1bb43072291d0480eb35
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-07-01 20:51:53 +0300

    added video link page

--- accept.php
+++ accept.php
@@ -25,7 +25,7 @@
     </style>
     <script type="text/javascript">
         setTimeout(function() {
-            window.location.href = "index.html";
+            window.location.href = "video-link.html";
         }, 10000); // 10000 milliseconds = 10 seconds
     </script>
 </head>
@@ -35,7 +35,7 @@
             <h1>Payment Successful</h1>
         </div>
         <div class="info">
-            <p>Thank you for your purchase! We have received your order and are processing it. Please check your email for payment confirmation and further instructions. You will be redirected to our homepage in 10 seconds.</p>
+            <p>Thank you for your purchase! We have received your order and are processing it. Please check your email for payment confirmation and further instructions. You will be redirected to our video link page in 10 seconds.</p>
         </div>
     </div>
 </body>
