4069ef33d9e77d612036f585e7905137c4c02051
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-07-07 22:02:41 +0300

    bug fix

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
-    return false; 
-} else {
-    include_once("resources/templates/redirect.html");
-}
