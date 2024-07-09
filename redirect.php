e941a05cd2085659e9a81865458e75c550e7c523
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-07-09 20:38:02 +0300

    bug fix

--- redirect.php
+++ redirect.php
@@ -5,6 +5,9 @@ require_once('WebToPay.php');
 // FOR CREDENTIALS
 require_once 'const.php';
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
 try {
     // Set PHP timezone to match your local timezone
     date_default_timezone_set('Europe/Vilnius'); // Adjust to your local timezone
